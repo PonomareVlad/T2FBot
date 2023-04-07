3a480ba0c295aa8b2f35a6d8b33b6631b843ab5d
Vladislav Ponomarev <PonomareVlad@users.noreply.github.com>
2023-04-08 01:29:35 +0500

    Prototype

--- src/bot.mjs
+++ src/bot.mjs
@@ -1,7 +1,34 @@
-import TeleBot from "telebot"
+import {parseCommands, keyboardGrid} from "telebot-utils";
+import TeleBot from "telebot";
 
-const bot = new TeleBot(process.env.TELEGRAM_BOT_TOKEN)
+const info = `Send any text content to get it as file`;
 
-bot.on("text", msg => msg.reply.text(msg.text))
+const extensions = ["txt", "md", "csv", "json", "html", "css", "svg", "js"];
+
+const bot = new TeleBot(process.env.TELEGRAM_BOT_TOKEN);
+
+bot.mod("message", parseCommands);
+
+bot.on("text", ({isCommand, reply}) => {
+    if (isCommand) return reply.text(info);
+    const buttons = extensions.map(callback => bot.inlineButton(callback, {callback}));
+    const replyMarkup = bot.inlineKeyboard(keyboardGrid(buttons, 4));
+    return reply.text(`Select extension for file:`, {asReply: true, replyMarkup});
+});
+
+bot.on("callbackQuery", async ({
+                                   id,
+                                   data,
+                                   message: {reply_to_message: {text}, message_id: messageId, chat: {id: chatId}}
+                               }) => {
+    if (!extensions.includes(data)) return bot.answerCallbackQuery(id, {text: "Wrong extension !"});
+    const fileName = `file.${data}`;
+    return Promise.all([
+        bot.answerCallbackQuery(id, {text: "Uploading file..."}),
+        bot.sendAction(chatId, "upload_document"),
+        bot.sendDocument(chatId, new Buffer(text), {fileName}),
+        bot.editMessageText({chatId, messageId}, `Generated file:`),
+    ]).catch(console.error);
+});
 
 export default bot
